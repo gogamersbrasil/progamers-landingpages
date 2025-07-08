b4ff843a8f4e30df9083ada785f43130608bd77d
Pedro-Roger <eng.pedrodev@gmail.com>
2025-07-08 11:16:09 -0300

    events initilized

--- src/components/producers components/NextEvent.tsx
+++ src/components/producers components/NextEvent.tsx
@@ -1,4 +1,25 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export default function NextEvent() {
+
+  const [event, setEvent] = useState([]);
+
+useEffect(() => {
+  async function fetchNextEvents() {
+    try {
+      const response = await axios.get(
+        "https://api.progamers.com.br/public/event/next/events"
+      );
+      setEvent(response.data);
+    } catch (error) {
+      console.error("Erro ao buscar campeonatos:", error);
+    }
+  }
+
+  fetchNextEvents();
+}, []);
+
   return (
     <section className="bg-gaming-darker flex flex-col items-center justify-center">
       <div className="relative z-10 text-center px-4 py-15">
